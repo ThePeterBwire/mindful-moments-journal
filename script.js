0e8051cc17e64a6543f217e90bfca752245707ec
ThePeterBwire <peterbwirea@gmail.com>
2025-03-26 09:04:19 +0300

    Ádded my DOM elements

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const journalForm = document.getElementById('journal-form');
+const entriesList = document.getElementById('entries-list');
+const quoteText = document.getElementById('quote-text');
+const newQuoteBtn = document.getElementById('new-quote-btn');
+const filterButtons = document.querySelectorAll('.filter-btn');
+
