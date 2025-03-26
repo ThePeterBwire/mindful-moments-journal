268b1458bb54229100ee8072fc4f5b606074ccf4
ThePeterBwire <peterbwirea@gmail.com>
2025-03-26 11:49:34 +0300

    Adds a function that fetches random quotes from the API

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const quoteText = document.getElementById('quote-text');
 const newQuoteBtn = document.getElementById('new-quote-btn');
 const filterButtons = document.querySelectorAll('.filter-btn');
 
+function fetchQuote() {
+  fetch('https://zenquotes.io/api/random')
+  .then(response => response.json())
+  .then(data => {
+    quoteText.textContent = `"${data[0].q}" - ${data[0].a}`;
+  })
+  .catch(() => {
+    quoteText.textContent = "Today is a gift that's why we call it the present!";
+  });
+}
+
